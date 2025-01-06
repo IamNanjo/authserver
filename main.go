d6eb354d7b4271421e9d32975353c1d5267e0512
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-06 12:37:18 +0200

    Embed static files and start backend

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"embed"
+	"github.com/IamNanjo/authserver/backend"
+	"os"
+)
+
+//go:embed static/*
+var staticFiles embed.FS
+
+func main() {
+	addr := os.Getenv("AUTH_SERVER_PORT")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	sFiles, err := staticFiles.ReadDir("static")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range sFiles {
+		os.Stdout.WriteString("./" + file.Name() + "\n")
+	}
+
+	os.Stdout.WriteString("Listening on " + addr + "\n")
+	backend.StartServer(addr, staticFiles)
+}
