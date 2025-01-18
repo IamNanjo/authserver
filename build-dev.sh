7837a5ba610ece913c423e2e42b7609f14d1fd2a
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-18 22:02:27 +0200

    build: More verbose output

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+NC='\033[0m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+echo -e "${YELLOW}Generating templ pages$NC" &&
 templ generate &&
 go mod tidy &&
-go build -o dist/
+echo -e "${YELLOW}Building go packages$NC" &&
+go build -o dist/ -v &&
+echo -e "${GREEN}All built$NC" &&
+echo -e "${YELLOW}Starting server$NC" &&
 dist/authserver
--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/env bash
 
-templ generate
+NC='\033[0m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+echo -e "${YELLOW}Generating templ pages$NC"
+templ generate &&
+echo &&
 go mod tidy
 
 if [ "$1" = "--skiptests" ] || [ "$1" = "-st" ]; then
 	echo "Skipping tests"
 else 
+	echo -e "${YELLOW}Running tests$NC"
 	go test ./...
 
 	if [ $? -ne 0 ]; then
@@ -17,6 +24,8 @@ else
 fi
 
 platforms=("linux/amd64" "linux/arm64" "windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64")
+
+echo -e "${YELLOW}Building project for ${#platforms[@]} platforms$NC"
 
 for platform in "${platforms[@]}"
 do
@@ -30,12 +39,14 @@ do
 		output_name+=".exe"
 	fi
 
+	printf "Building $output_name - "
+
 	env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
 
 	if [ $? -ne 0 ]; then
-   		echo "Failed to build for $platform"
+   		echo "failed"
 		exit 1
 	else
-		echo "Built $output_name"
+		echo -e "${GREEN}ok$NC"
 	fi
 done
