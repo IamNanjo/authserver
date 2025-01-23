40395f4076274a180636b5b9005d5cabf5133364
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-24 01:35:25 +0200

    feat: Parse DB path from environment variables and CLI flags

--- main.go
+++ main.go
@@ -12,17 +12,25 @@ import (
 var staticFiles embed.FS
 
 func main() {
+	var err error
 	addr := os.Getenv("AUTH_SERVER_PORT")
 	if addr == "" {
 		addr = ":8080"
 	}
 
-	// Override environment variable with cli flag
+	dbPath := os.Getenv("AUTH_SERVER_DB")
+
+	// Override environment variables with cli flag
 	flag.StringVar(&addr, "addr", addr, "Listen address")
+	flag.StringVar(&dbPath, "db", dbPath, "Database path")
 
 	flag.Parse()
 
-	err := db.Initialize(nil)
+	if dbPath != "" {
+		err = db.Initialize(&dbPath)
+	} else {
+		err = db.Initialize(nil)
+	}
 
 	if err != nil {
 		os.Stderr.WriteString("Could not initialize database. Error: " + err.Error() + "\n")
