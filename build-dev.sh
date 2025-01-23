8cec743fe7c963ed5006b585c7d25280adf4d54f
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-24 01:26:49 +0200

    build: Test before build

--- build-dev.sh
+++ build-dev.sh
@@ -7,6 +7,21 @@ GREEN='\033[0;32m'
 echo -e "${YELLOW}Generating templ pages$NC" &&
 templ generate &&
 go mod tidy &&
+
+if [ "$1" = "--skiptests" ] || [ "$1" = "-st" ]; then
+	echo "Skipping tests"
+else 
+	echo -e "${YELLOW}Running tests$NC"
+	go test ./...
+
+	if [ $? -ne 0 ]; then
+		echo "Tests failed. Fix issues first"
+		exit 1
+	else
+		echo
+	fi
+fi
+
 echo -e "${YELLOW}Building go packages$NC" &&
 go build -o dist/ -v &&
 echo -e "${GREEN}All built$NC" &&
