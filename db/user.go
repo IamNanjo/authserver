f4ebef069553ebeaeb6cfb3e1c98687498c54cfb
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-25 13:44:10 +0200

    Added user creation

--- db/user.go
+++ db/user.go
@@ -1,5 +1,10 @@
 package db
 
+import (
+	"errors"
+	"github.com/IamNanjo/authserver/hash"
+)
+
 func GetUserByEmailOrUsername(emailOrUsername string) (User, error) {
 	user := User{}
 	err := Connection().Get(&user, `
@@ -31,4 +36,50 @@ func GetAppUsers(id string) ([]UserWithAppRole, error) {
 	)
 
 	return users, err
+}
+
+// Returns new user ID
+func CreateUser(name string, email string, password string) (string, error) {
+	connection := Connection()
+
+	var remainingAttempts = 5
+	var id = ""
+	var err error
+
+	for {
+		if remainingAttempts == 0 {
+			return id, errors.New("Could not create unique ID")
+		}
+
+		id, err = GenerateId(10)
+		if err != nil {
+			return id, err
+		}
+
+		err = connection.Get(nil, "SELECT 1 FROM User WHERE id = $1", id)
+		if err != nil {
+			break
+		}
+
+		remainingAttempts--
+	}
+
+	hashedPassword, err := hash.Hash([]byte(password), nil)
+	if err != nil {
+		return id, err
+	}
+
+	tx, err := connection.Begin()
+	if err != nil {
+		return id, err
+	}
+
+	_, err = tx.Exec("INSERT INTO User (id, name, email, password) VALUES ($1, $2, $3, $4)", id, name, email, hashedPassword)
+	if err != nil {
+
+	}
+
+	err = tx.Commit()
+
+	return id, err
 }
