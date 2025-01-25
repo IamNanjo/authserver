43beb7931fe7bdbfa78e1fb179648594f92d7d70
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-25 15:18:12 +0200

    Initialize WebAuthn

--- backend/api/passkey.go
+++ backend/api/passkey.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+var WebAuthnConfig = &webauthn.Config{
+	RPDisplayName: "Auth Service",
+	RPID:          "auth.nanjo.dev",
+	RPOrigins:     []string{"https://auth.nanjo.dev"},
+}
+
+var WebAuthn *webauthn.WebAuthn
+
 func PasskeyOptions(w http.ResponseWriter, r *http.Request) {
 
 }
--- backend/backend.go
+++ backend/backend.go
@@ -3,11 +3,19 @@ package backend
 import (
 	"github.com/IamNanjo/authserver/backend/api"
 	"github.com/IamNanjo/authserver/backend/routes"
+	"github.com/go-webauthn/webauthn/webauthn"
 	"io/fs"
 	"net/http"
 )
 
 func StartServer(addr string, staticFiles fs.FS) {
+	webAuthn, err := webauthn.New(api.WebAuthnConfig)
+	if err != nil {
+		panic("Could not initialize WebAuthn")
+	}
+
+	api.WebAuthn = webAuthn
+
 	http.Handle("/static/", http.FileServer(http.FS(staticFiles)))
 
 	http.HandleFunc("/api/auth/password", api.PasswordAuth)
