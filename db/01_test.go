262b0dfbea9ce816ab7cbcacc7d6f4e53b3c7f7e
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-25 13:40:08 +0200

    test: Setup database tests

---
+++ db/01_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestSetup(t *testing.T) {
+	dbPath, _ = os.Getwd()
+	dbPath = strings.TrimSuffix(dbPath, "db")
+	dbPath += "dist/authserver_test" + ".db"
+
+	err := Initialize(&dbPath)
+	if err != nil {
+		t.Errorf("Database initialization failed. %v", err)
+	}
+
+	// Truncate tables
+	Connection().Exec(`
+		DELETE FROM App;
+		DELETE FROM Domain;
+		DELETE FROM User;
+		DELETE FROM AppUser;
+		`)
+}
