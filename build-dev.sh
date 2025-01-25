4e1c794b143196f098fa41243eeddb09133ebc31
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-25 12:13:28 +0200

    build: Print local test URL

--- build-dev.sh
+++ build-dev.sh
@@ -3,6 +3,7 @@
 NC='\033[0m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 
 echo -e "${YELLOW}Generating templ pages$NC" &&
 templ generate &&
@@ -15,15 +16,22 @@ else
 	go test -count=1 ./...
 
 	if [ $? -ne 0 ]; then
-		echo "Tests failed. Fix issues first"
+		echo -e "${RED}Tests failed. Fix issues first$NC"
 		exit 1
 	else
 		echo
 	fi
 fi
 
-echo -e "${YELLOW}Building go packages$NC" &&
+echo -e "${YELLOW}Building go packages$NC"
 go build -o dist/ -v &&
-echo -e "${GREEN}All built$NC" &&
-echo -e "${YELLOW}Starting server$NC" &&
+echo -e "${GREEN}All built$NC"
+
+echo -e "${YELLOW}Starting server$NC"
+
+if command -v sqlite3 &> /dev/null; then
+	appId=$(sqlite3 -column dist/authserver_test.db "SELECT id FROM App;")
+	echo "https://local.test?app=$appId&redirect=https://local.test"
+fi
+
 dist/authserver
