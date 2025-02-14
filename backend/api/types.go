51458b012c73349e5b36f1cce60ba05013baefd0
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-02-14 03:35:51 +0200

    chore: Added common types for API

---
+++ backend/api/types.go
@@ -0,0 +1,35 @@
+package api
+
+import "github.com/go-webauthn/webauthn/webauthn"
+
+type User struct {
+	Id          []byte
+	Name        string
+	Credentials []webauthn.Credential
+}
+
+func (u User) WebAuthnID() []byte {
+	return u.Id
+}
+
+func (u User) WebAuthnName() string {
+	return u.Name
+}
+
+func (u User) WebAuthnDisplayName() string {
+	return u.Name
+}
+
+func (u User) WebAuthnCredentials() []webauthn.Credential {
+	return u.Credentials
+}
+
+type EmailAndUsernameRequestBody struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
+
+type ErrorResponse struct {
+	Reason string `json:"reason"`
+	Error  string `json:"error"`
+}
