4bfbbc1f0fbcb66988b8478adc30663d5997fd03
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-04-19 16:19:22 +0300

    feat: Added new util function for sending error responses

---
+++ backend/utils/error.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/IamNanjo/authserver/components"
+)
+
+func Error(w http.ResponseWriter, r *http.Request, statusCode int, message string) {
+	accept := r.Header.Get("Accept")
+
+	if r.Header.Get("HX-Request") != "" {
+		accept = "text/html"
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(statusCode)
+	}
+
+	switch accept {
+	case "text/html":
+		components.Error(message, nil).Render(r.Context(), w)
+	default:
+		json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+	}
+}
