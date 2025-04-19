99cc3fa796ec1e59e1fee9e2dc59938ee5642471
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-04-19 16:21:04 +0300

    fix: Correctly handle conflicts

--- backend/api/register.go
+++ backend/api/register.go
@@ -1,38 +1,47 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 	"strings"
 
 	"github.com/IamNanjo/authserver/backend/utils"
-	"github.com/IamNanjo/authserver/components"
 	"github.com/IamNanjo/authserver/db"
 )
 
 func PasswordRegister(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	expectsJsonResponse := strings.HasPrefix(r.Header.Get("Accept"), "application/json")
-
 	email := r.PostForm.Get("email")
 	username := r.PostForm.Get("username")
 	password := r.PostForm.Get("password")
 
 	if username == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		errorMessage := "No username provided"
-		if expectsJsonResponse {
-			json.NewEncoder(w).Encode(ErrorResponse{Reason: "username", Error: errorMessage})
-		} else {
-			components.Error(errorMessage, nil).Render(r.Context(), w)
-		}
+		utils.Error(w, r, http.StatusBadRequest, "No username provided")
 		return
 	}
 
 	_, err := db.CreateUser(username, email, password)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		errMsg := err.Error()
+		after, conflict := strings.CutPrefix(errMsg, "constraint failed: UNIQUE constraint failed: User.")
+
+		if !conflict {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		reason := strings.Split(after, " ")[0]
+		errorMessage := "Unknown conflict with existing user"
+
+		switch reason {
+		case "name":
+			errorMessage = "A user with this username already exists"
+		case "email":
+			errorMessage = "A user with this email already exists"
+		}
+
+		utils.Error(w, r, http.StatusConflict, errorMessage)
+
 		return
 	}
 
